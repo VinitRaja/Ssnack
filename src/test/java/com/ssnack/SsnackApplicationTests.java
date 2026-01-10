0580223a75e241e63a557810411682ddb253d3ae
Vinit Raja <60826741+VinitRaja@users.noreply.github.com>
2026-01-11 01:23:09 +0530

    Add SsnackApplicationTests for application context

---
+++ src/test/java/com/ssnack/SsnackApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ssnack;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+  class SsnackApplicationTests{
+    @Test
+    void contextLoads(){
+      
+    }
+  }

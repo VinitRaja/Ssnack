d1c258790f3fc97a6174c546d5b51d76ff0fac56
Vinit Raja <60826741+VinitRaja@users.noreply.github.com>
2026-01-12 02:23:35 +0530

    Created UserServiceImpl interface

---
+++ src/main/java/com/ssnack/service/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ssnack.service;
+
+import com.ssnack.model.Item;
+import com.ssnack.model.User;
+import com.ssnack.repository.ItemRepository;
+import com.ssnack.repository.UserRepository;
+import java.Util.List;
+import java.Util.Optional;
+
+public interface UserServiceImpl{
+  int MAX_ITEMS = 10;
+  
+}

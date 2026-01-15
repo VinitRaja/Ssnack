14a74da667cd14ad82b4a6538efaf972219ee721
Vinit Raja <60826741+VinitRaja@users.noreply.github.com>
2026-01-16 04:20:48 +0530

    Change allowedCredentials type to boolean and parse value

--- src/main/java/com/ssnack/config/WebConfig.java
+++ src/main/java/com/ssnack/config/WebConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     private String allowedHeaders;
 
     @Value("${app.cors.allowed-credentials}")
-    private String allowedCredentials;
+    private boolean allowedCredentials;
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
@@ -26,6 +26,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
               .allowedOrigins(allowedOrigins)
               .allowedMethods(allowedMethods)
               .allowedHeaders(allowedHeaders)
-              .allowedCredentials(allowedCredentials);
+              .allowedCredentials(Boolean.parseBoolean(allowedCredentials));
     }
   }
